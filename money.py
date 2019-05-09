a5e2c18fac12733b5de14b0767959c5602e22fef
UrangUrang <ddwax89@gmail.com>
2019-05-09 23:51:40 +0900

    Add Franc model, but it is copied from Dollar so there are many things duplicated

--- money.py
+++ money.py
@@ -7,4 +7,19 @@ class Dollar(object):
         return Dollar(self.__amount * multiplier)
 
     def __eq__(self, other):
+        if not isinstance(other, Dollar):
+            raise TypeError
+        return self.__amount == other.__amount
+
+
+class Franc(object):
+    def __init__(self, amount):
+        self.__amount = amount
+
+    def times(self, multiplier):
+        return Franc(self.__amount * multiplier)
+
+    def __eq__(self, other):
+        if not isinstance(other, Franc):
+            raise TypeError
         return self.__amount == other.__amount
