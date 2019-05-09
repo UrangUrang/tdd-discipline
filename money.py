f351ea5ea7a94f0134a3545c685d2889accfc405
UrangUrang <ddwax89@gmail.com>
2019-05-10 00:35:36 +0900

    Make parent class Money for Dollar, Franc. And commonized __eq__(), I think times also can be commonized too

--- money.py
+++ money.py
@@ -1,25 +1,26 @@
-class Dollar(object):
 
+class Money(object):
     def __init__(self, amount):
-        self.__amount = amount
-
-    def times(self, multiplier):
-        return Dollar(self.__amount * multiplier)
+        self.amount = amount
 
     def __eq__(self, other):
-        if not isinstance(other, Dollar):
+        if not isinstance(other, Money):
             raise TypeError
-        return self.__amount == other.__amount
+        return self.amount == other.amount
 
 
-class Franc(object):
+class Dollar(Money):
+
     def __init__(self, amount):
-        self.__amount = amount
+        super().__init__(amount)
 
     def times(self, multiplier):
-        return Franc(self.__amount * multiplier)
+        return Dollar(self.amount * multiplier)
 
-    def __eq__(self, other):
-        if not isinstance(other, Franc):
-            raise TypeError
-        return self.__amount == other.__amount
+
+class Franc(Money):
+    def __init__(self, amount):
+        super().__init__(amount)
+
+    def times(self, multiplier):
+        return Franc(self.amount * multiplier)
--- test_money.py
+++ test_money.py
@@ -1,4 +1,4 @@
-from money import Dollar, Franc
+from money import Dollar, Franc, Money
 
 
 def test_multiplication():
@@ -14,8 +14,8 @@ def test_franc_multiplication():
 
 
 def test_equality():
-    assert Dollar(5).__eq__(Dollar(5)) is True
     assert (Dollar(5) == Dollar(5)) is True
-    assert Dollar(5).__eq__(Dollar(6)) is False
     assert (Dollar(5) == Dollar(6)) is False
+    assert (Franc(5) == Franc(5)) is True
+    assert (Franc(5) == Franc(6)) is False
 
